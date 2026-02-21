519f23a41652ea272cffc4fd8a80215bbe80e778
CatV2004 <nguyenmanhc261@gmail.com>
2026-02-21 17:18:17 +0700

    init project springboot

---
+++ urban-management/src/main/java/com/smartcity/urban_management/UrbanManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.urban_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanManagementApplication.class, args);
+	}
+
+}
